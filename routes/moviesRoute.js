50d0580b6592fe424ceb930bc3fae004f4d0b40f
ParamPS25 <bhavsarparam1940@gmail.com>
2024-10-06 00:53:33 +0530

    pagination

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -4,10 +4,10 @@ const Movie = require("../models/movieModel");
 
 const router = express.Router();
 
-// router.get("/",async(req,res)=>{
-//     // await Movie.create(movieJson).then(()=>{
-//     //     res.json({msg:"success"});
-//     // })
+//router.get("/",async(req,res)=>{
+//     await Movie.create(movieJson).then(()=>{
+//         res.json({msg:"success"});
+//     })
 // });
 
 // as req.query is key value pair req.query[key] = value  eg ?year=2007 so,obj[year] gives 2007
@@ -20,10 +20,19 @@ router.get("/",async(req,res)=>{
         let sortFix = '';
         let selectFlag = false;
         let selectFix = '';
+
+        let page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        let skip = (page - 1) * limit;
+
+
         let query = {};
         // Apply regex to string fields and handle numeric fields separately
 
         for (let key in req.query) {
+            if (key === 'page' || key === 'limit') {
+                continue;
+            }
             if (isNaN(req.query[key]) && key != 'genres' && key != 'sort' && key != 'select') {        // Apply regex for string fields
                 query[key] = { $regex: req.query[key], $options: "i" };
             }
@@ -47,19 +56,23 @@ router.get("/",async(req,res)=>{
                 query[key] = Number(req.query[key]);
             }
 
-            console.log(`Query for ${key}:`, query[key]);
+            console.log(`Query for ${key}:`, query[key]);   // query must be undefined for sort select page limit skip as it is queryObj passing to find required documents in Movie model whereas select,sort,page,limit are function which are applied on it !!
         }
         console.log(query);
         //const allMovies = await Movie.find(req.query);   //.find({}) all movies || ({year=2007}) queried movie
 
         let allMovies;
+        if(skip != null){
         if(sortFlag){
-            allMovies = selectFlag ? await Movie.find(query).sort(sortFix).select(selectFix)
-                                    : await Movie.find(query).sort(sortFix);
+            allMovies = selectFlag ? await Movie.find(query).sort(sortFix).select(selectFix).skip(skip).limit(limit)
+                                    : await Movie.find(query).sort(sortFix).skip(skip).limit(limit);;
         }else{
-            allMovies = selectFlag ? await Movie.find(query).select(selectFix)
-                                    : await Movie.find(query);
+            allMovies = selectFlag ? await Movie.find(query).select(selectFix).skip(skip).limit(limit)
+                                    : await Movie.find(query).skip(skip).limit(limit);
         }
+    }else{
+        allMovies = await Movie.find(query);
+    }
         res.json({allMovies:allMovies});
     
     }catch(e){
